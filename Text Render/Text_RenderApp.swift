26cca47d19eff05d009e18c62cc6d9791a2d362a
uwi0 <kakapoweb@gmail.com>
2024-08-27 14:16:39 +0700

    Initial Commit

---
+++ Text Render/Text_RenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Text_RenderApp.swift
+//  Text Render
+//
+//  Created by dwi prasetyo on 27/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Text_RenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
